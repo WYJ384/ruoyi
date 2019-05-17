90ea3b989d680d95e4d2bee4c23a06d0f456e60c
mazhongyi <mzy620524>
2019-05-17 17:14:29 +0800

    导入

--- ruoyi-admin/src/main/java/com/ruoyi/web/frontcontroller/FIndexController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/frontcontroller/FIndexController.java
@@ -30,6 +30,32 @@ public class FIndexController extends FBaseController{
         startPage(1,6,"hits desc");
         List<Article> hotArticles = articleService.selectArticleList(article);
         modelMap.addAttribute("hotArticles",hotArticles);
+
+        article=new Article();
+        article.setCategoryId("fd260cf5199b4d4191fa44693ce71ba7");
+        startPage(1,6,"hits desc");
+        List<Article> yunArticle = articleService.selectArticleList(article);
+        modelMap.addAttribute("yunArticle",yunArticle);
+
+
+        article=new Article();
+        article.setCategoryId("01f53126cda445d1a48ab3e3abf264f8");
+        startPage(1,6,"hits desc");
+        List<Article> chuanshuArticle = articleService.selectArticleList(article);
+        modelMap.addAttribute("chuanshuArticle",chuanshuArticle);
+
+
+        article=new Article();
+        article.setCategoryId("2e841af613aa4e4292a24d9e6bd25c74");
+        startPage(1,6,"hits desc");
+        List<Article> shujuArticle = articleService.selectArticleList(article);
+        modelMap.addAttribute("shujuArticle",shujuArticle);
+        article=new Article();
+        article.setCategoryId("3205c97eab044e62a27b45276b74a1c0");
+        startPage(1,6,"hits desc");
+        List<Article> iptvArticle = articleService.selectArticleList(article);
+        modelMap.addAttribute("iptvArticle",iptvArticle);
+
         return "/front/index" ;
     }
 
