99c126c51dbefa3159bc297c7cde1aca28d97c91
mazhongyi <mzy620524>
2019-06-10 09:58:38 +0800

    QC

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/worktask/QCController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/worktask/QCController.java
@@ -1,33 +1,29 @@
 package com.ruoyi.web.controller.worktask;
 
-import java.util.List;
-import java.util.UUID;
-
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.util.ShiroUtils;
 import com.ruoyi.system.domain.SysUser;
 import com.ruoyi.system.service.ISysUserService;
+import com.ruoyi.worktask.domain.SelfTask;
 import com.ruoyi.worktask.domain.SelfTaskProcess;
 import com.ruoyi.worktask.domain.WorkTaskFile;
 import com.ruoyi.worktask.service.ISelfTaskProcessService;
+import com.ruoyi.worktask.service.ISelfTaskService;
 import com.ruoyi.worktask.service.IWorkTaskFileService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.worktask.domain.SelfTask;
-import com.ruoyi.worktask.service.ISelfTaskService;
-import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.core.page.TableDataInfo;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * 任务 信息操作处理
@@ -36,10 +32,10 @@ import com.ruoyi.common.utils.poi.ExcelUtil;
  * @date 2019-05-08
  */
 @Controller
-@RequestMapping("/worktask/selfTask")
-public class SelfTaskController extends BaseController
+@RequestMapping("/worktask/qc")
+public class QCController extends BaseController
 {
-    private String prefix = "worktask/selfTask";
+    private String prefix = "worktask/qc";
 	@Autowired
 	private IWorkTaskFileService workTaskFileService;
 	@Autowired
@@ -48,14 +44,14 @@ public class SelfTaskController extends BaseController
 	private ISelfTaskProcessService selfTaskProcessService;
 	@Autowired
 	private ISysUserService userService;
-	String seTaskType="1";
+	String seTaskType="2";
 	@RequiresPermissions("worktask:selfTask:view")
 	@GetMapping()
 	public String selfTask()
 	{
-	    return prefix + "/selfTask";
+		return prefix + "/selfTask";
 	}
-	
+
 	/**
 	 * 我创建的
 	 */
@@ -67,7 +63,7 @@ public class SelfTaskController extends BaseController
 		startPage();
 		selfTask.setSelvalTaskType(seTaskType);
 		selfTask.setCreateBy(ShiroUtils.getUserId()+"");
-        List<SelfTask> list = selfTaskService.selectSelfTaskList(selfTask);
+		List<SelfTask> list = selfTaskService.selectSelfTaskList(selfTask);
 		return getDataTable(list);
 	}
 	/**
@@ -103,15 +99,15 @@ public class SelfTaskController extends BaseController
 	 * 导出任务列表
 	 */
 	@RequiresPermissions("worktask:selfTask:export")
-    @PostMapping("/export")
-    @ResponseBody
-    public AjaxResult export(SelfTask selfTask)
-    {
-    	List<SelfTask> list = selfTaskService.selectSelfTaskList(selfTask);
-        ExcelUtil<SelfTask> util = new ExcelUtil<SelfTask>(SelfTask.class);
-        return util.exportExcel(list, "selfTask");
-    }
-	
+	@PostMapping("/export")
+	@ResponseBody
+	public AjaxResult export(SelfTask selfTask)
+	{
+		List<SelfTask> list = selfTaskService.selectSelfTaskList(selfTask);
+		ExcelUtil<SelfTask> util = new ExcelUtil<SelfTask>(SelfTask.class);
+		return util.exportExcel(list, "selfTask");
+	}
+
 	/**
 	 * 新增任务
 	 */
@@ -122,15 +118,15 @@ public class SelfTaskController extends BaseController
 		modelMap.addAttribute("sysUsers",sysUsers);
 		return prefix + "/add";
 	}
-    @GetMapping("/addChild/{pid}")
-    public String addChild(ModelMap modelMap,@PathVariable("pid")  String pid)
-    {
-        List<SysUser> sysUsers = userService.selectUserList(new SysUser());
-        modelMap.addAttribute("sysUsers",sysUsers);
-        modelMap.addAttribute("pid",pid);
-        return prefix + "/add";
-    }
-	
+	@GetMapping("/addChild/{pid}")
+	public String addChild(ModelMap modelMap,@PathVariable("pid")  String pid)
+	{
+		List<SysUser> sysUsers = userService.selectUserList(new SysUser());
+		modelMap.addAttribute("sysUsers",sysUsers);
+		modelMap.addAttribute("pid",pid);
+		return prefix + "/add";
+	}
+
 	/**
 	 * 新增保存任务
 	 */
@@ -142,7 +138,7 @@ public class SelfTaskController extends BaseController
 	{
 		selfTask.setSelvalTaskType(seTaskType);
 		selfTask.setTaskStatus("0");
-        selfTask.setTaskLevel("0");
+		selfTask.setTaskLevel("0");
 		selfTask.setCreateBy(ShiroUtils.getUserId()+"");
 		selfTask.setId(UUID.randomUUID().toString().replaceAll("-",""));
 		return toAjax(selfTaskService.insertSelfTask(selfTask));
@@ -162,7 +158,7 @@ public class SelfTaskController extends BaseController
 		List<SelfTaskProcess> processList = selfTaskProcessService.selectSelfTaskProcessList(selfTaskProcess);
 		mmap.put("processList", processList);
 		mmap.put("selfTask", selfTask);
-	    return prefix + "/edit";
+		return prefix + "/edit";
 	}
 	@GetMapping("/query/{id}/{taskType}")
 	public String query(@PathVariable("id") String id,@PathVariable("taskType") String taskType, ModelMap mmap)
@@ -173,18 +169,18 @@ public class SelfTaskController extends BaseController
 		SelfTaskProcess selfTaskProcess=new SelfTaskProcess();
 		selfTaskProcess.setId(id);
 		List<SelfTaskProcess> processList = selfTaskProcessService.selectSelfTaskProcessList(selfTaskProcess);
-        SelfTask childSelfTask=new SelfTask();
-        childSelfTask.setPid(id);
-        List<SelfTask> childSelfTasks = selfTaskService.selectSelfTaskList(childSelfTask);
+		SelfTask childSelfTask=new SelfTask();
+		childSelfTask.setPid(id);
+		List<SelfTask> childSelfTasks = selfTaskService.selectSelfTaskList(childSelfTask);
 
 		WorkTaskFile activityFile=new WorkTaskFile();
 		activityFile.setWorkTaskId(id);
 		List<WorkTaskFile> workTaskFiles = workTaskFileService.selectWorkTaskFileList(activityFile);
 		mmap.put("workTaskFiles", workTaskFiles);
-        mmap.put("taskType", taskType);
+		mmap.put("taskType", taskType);
 		mmap.put("processList", processList);
 		mmap.put("selfTask", selfTask);
-        mmap.put("childSelfTasks", childSelfTasks);
+		mmap.put("childSelfTasks", childSelfTasks);
 		return prefix + "/query";
 	}
 	/**
@@ -206,7 +202,7 @@ public class SelfTaskController extends BaseController
 		}
 		return toAjax(selfTaskService.updateSelfTask(selfTask));
 	}
-	
+
 	/**
 	 * 删除任务
 	 */
@@ -215,8 +211,11 @@ public class SelfTaskController extends BaseController
 	@PostMapping( "/remove")
 	@ResponseBody
 	public AjaxResult remove(String ids)
-	{		
+	{
 		return toAjax(selfTaskService.deleteSelfTaskByIds(ids));
 	}
-	
+
 }
+
+	
+
